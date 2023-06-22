c68dbcfb17270c5c3c25997b4dc7c813ae0789c0
wimdavies <william.johnston.davies@gmail.com>
2023-06-22 11:20:13 +0100

    Refactored

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -3,9 +3,9 @@
 def leap_year?(year)
   if year % 400 == 0
     true
-  elsif year % 100 == 0 && year % 400 > 0
+  elsif year % 100 == 0
     false
-  elsif year % 4 == 0 && year % 100 > 0
+  elsif year % 4 == 0
     true
   else
     false
